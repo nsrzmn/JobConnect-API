a68961e9016a0b06c113499f3eb42b8ca7e0b9ee
Nasir <nsrzmn@gmail.com>
2025-03-07 19:40:21 +0100

    Update database configuration to use environment variables for MongoDB connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const uri = `mongodb+srv://nasir:nasir@jobconnect-api.eiifs.mongodb.net/Jobconnect?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@jobconnect-api.eiifs.mongodb.net/Jobconnect?retryWrites=true&w=majority`;
 
 export const connectDB = async () => {
   try {
@@ -10,6 +13,9 @@ export const connectDB = async () => {
     } as any);
     console.log("✅ MongoDB Connected!");
   } catch (error) {
+    console.log("username: ",process.env.DATABASE_USERNAME);
+    console.log("password: ",process.env.DATABASE_PASSWORD);
+    
     console.error("❌ Error connecting to MongoDB:", error);
     process.exit(1); // Exit process with failure
   }
